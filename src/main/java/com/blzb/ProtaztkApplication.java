b8357f8937ea0078a7bc137cd9d1b8810da7638c
Angel Pimentel <angelpmza@gmail.com>
2017-04-27 20:09:52 -0500

    Added springfox

--- src/main/java/com/blzb/ProtaztkApplication.java
+++ src/main/java/com/blzb/ProtaztkApplication.java
@@ -1,9 +1,25 @@
 package com.blzb;
 
+import com.google.common.base.Predicate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.*;
 
 @SpringBootApplication
+@Import({ springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class})
 public class ProtaztkApplication {
 
 	public static void main(String[] args) {
