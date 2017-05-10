06f07dad362601bdd5c0990e43047a822f53a533
Angel Pimentel <angelpmza@gmail.com>
2017-05-10 11:14:17 -0500

    Fixed estados

--- src/main/java/com/blzb/data/dbo/Estado.java
+++ src/main/java/com/blzb/data/dbo/Estado.java
@@ -15,6 +15,54 @@ public class Estado {
     private Date fecha;
     private float estatura;
     private float peso;
-    @OneToOne
+    @ManyToOne
     private Persona persona;
+
+    public Long getId() {
+        return id;
+    }
+
+    @PrePersist
+    void prePersist(){
+        if(fecha == null){
+            fecha = new Date();
+        }
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(float estatura) {
+        this.estatura = estatura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
 }
--- src/main/java/com/blzb/data/dbo/Persona.java
+++ src/main/java/com/blzb/data/dbo/Persona.java
@@ -24,7 +24,7 @@ public class Persona {
     @OneToMany(mappedBy = "persona", cascade = CascadeType.ALL)
     List<Marca> marcas;
 
-    @OneToMany
+    @OneToMany(mappedBy = "persona", cascade = CascadeType.ALL)
     List<Estado> estados;
 
     public Long getId() {
